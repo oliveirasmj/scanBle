89f309c2132abf590f694929c2b5747ee5fa31b8
Miguel Oliveira <oliveirasmj@gmail.com>
2023-04-13 17:40:51 +0100

    create folder temp

---
+++ teste9/scan.py
@@ -0,0 +1,162 @@
+import asyncio
+import platform
+import sys
+from bleak import BleakClient, BleakScanner
+
+import pymongo
+from pymongo import MongoClient
+
+import datetime
+
+
+cluster = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+db = cluster["dbscan"]
+
+class textcolor:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    CYAN = '\033[96m'
+    YELLOW = '\033[93m'
+    GOLD = '\033[33m'
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
+def device_details_to_dict(raw_details):
+    # Format device details into string. Accommodate errors caused by lack of data.
+    dict_ = {
+        'address': None,
+        'details': None,
+        'metadata': None,
+        'name': None,
+        'rssi': None
+    }
+    try:
+        dict_['address'] = raw_details.address
+    except Exception:
+        print(f'Address not found for device with the following data: {raw_details}')
+    try:
+        dict_['details'] = raw_details.details
+    except Exception:
+        print(f'Details not found for device with the following data: {raw_details}')
+    try:
+        dict_['metadata'] = raw_details.metadata
+    except Exception:
+        print(f'Metadata not found for device with the following data: {raw_details}')
+    try:
+        dict_['name'] = raw_details.name
+    except Exception:
+        print(f'Name not found for device with the following data: {raw_details}')
+    try:
+        dict_['rssi'] = raw_details.rssi
+    except Exception:
+        print(f'RSSI not found for device with the following data: {raw_details}')
+
+    return dict_
+
+
+async def main():
+    print('///////////////////////////////////////////////')
+    print('\t\tDevice scan')
+    print('///////////////////////////////////////////////')
+    print('Scanning for Bluetooth LE devices...')
+    devices = await BleakScanner.discover()
+    
+
+    print(f'Devices found (raw data):\n\t{devices}')
+    print(f'Number of devices found: {textcolor.CYAN}{len(list(devices))}{textcolor.END}')
+    print(f'Details of devices found:')
+
+    for device in devices:
+        device_dict = device_details_to_dict(device)
+        print('Device found:')
+        print(f'\tAddress: {textcolor.GOLD}{device.address}{textcolor.END}')
+        print(f'\tName: {textcolor.GREEN}{device.name}{textcolor.END}')
+        print(f'\tDetails: {device.details}')
+        print(f'\tMetadata: {device.metadata}')
+        print(f'\tRSSI: {device.rssi}')
+
+        #Guardar device na BD
+        #--------------------------------------------------------       
+        collection = db["devices"]
+        dado = {
+            'address': f'{device.address}',
+            'name': f'{device.name}',
+            'details': f'{device.details}',
+            'metadata': f'{device.metadata}',
+            'rssi': f'{device.rssi}',
+            'time': datetime.datetime.now()
+        }
+        collection.insert_one(dado)
+        #-------------------------------------------------------- 
+
+
+
+    
+    print('///////////////////////////////////////////////')
+    print('\t\tServices scan')
+    print('///////////////////////////////////////////////')
+    print('Requesting list of services and characteristics from found Bluetooth LE devices...')
+    for device in devices:
+        try:
+            temp_device = await BleakScanner.find_device_by_address(device.address, timeout=20)
+            async with BleakClient(temp_device) as client:
+                print('Services found for device')
+                print(f'\tDevice address: {textcolor.GOLD}{device.address}{textcolor.END}')
+                print(f'\tDevice name: {textcolor.GREEN}{device.name}{textcolor.END}')
+
+
+                #Guardar address e name na BD
+                #--------------------------------------------------------
+                collection = db["services"]
+                dado = {
+                    'address': f'{device.address}',
+                    'name': f'{device.name}',
+                    'time': datetime.datetime.now()
+
+                }
+                #Inserir e guardar id
+                _id = collection.insert_one(dado)
+                #--------------------------------------------------------
+
+
+
+                print("\tServices:")
+                for service in client.services:
+                    print('\t\tService')
+                    print(f'\t\tDescription: {textcolor.CYAN}{service.description}{textcolor.END}')
+                    print(f'\t\tService: {textcolor.GOLD}{service}{textcolor.END}')
+
+                    characteristics = []
+                    serviceDetails = {
+                        'description': f'{service.description}',
+                        'service': f'{service}'
+                    }
+
+                    for c in service.characteristics:
+                        characteristics.append([c.uuid, c.description, c.handle, c.properties])
+                        
+                        #escrever cada servico que existe para a BD
+                        #--------------------------------------------------------
+                        serviceDetails = {
+                            'description': f'{service.description}',
+                            'service': f'{service}',
+                            'uuid': f'{c.uuid}',
+                            'subDescription': f'{c.description}',
+                            'handle': f'{c.handle}',
+                            'properties': f'{c.properties}'
+                        }
+                    collection.update_many({'_id': _id.inserted_id}, { "$push": { "services": serviceDetails } })
+                    #--------------------------------------------------------
+
+                        
+                    print(f'\t\tCharacteristics: {characteristics}')
+
+
+        except asyncio.exceptions.TimeoutError:
+            print(f'{textcolor.RED}TimeoutError:{textcolor.END} Device at address `{device.address}` timed out.')
+        except Exception as error:
+            print(f'Exception: An error occurred while connecting to device `{device.address}`:\n\t{error}')
+    
+
+if __name__ == "__main__":
+    asyncio.run(main())
