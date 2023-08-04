a9231551f47f7cebe6a9ba34b4beef0726675e37
Miguel Oliveira <oliveirasmj@gmail.com>
2023-08-04 16:48:58 +0100

    added ram monitor

--- monitor.py
+++ monitor.py
@@ -2,24 +2,25 @@ import psutil
 import csv
 import time
 
-def monitorizar_cpu():
-    with open('uso_cpu.csv', mode='w', newline='') as csv_file:
-        fieldnames = ['Tempo (s)', 'Uso do CPU (%)']
+def monitorizar_recursos():
+    with open('uso_recursos.csv', mode='w', newline='') as csv_file:
+        fieldnames = ['Tempo (s)', 'Uso do CPU (%)', 'Uso de RAM (%)']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
 
         segundos = 0
         while True:
-            uso_cpu = psutil.cpu_percent(interval=1)
-            print(f'Tempo: {segundos}s | Uso do CPU: {uso_cpu:.2f}%')
+            uso_cpu = psutil.cpu_percent(interval=0.2)
+            uso_ram = psutil.virtual_memory().percent
+            print(f'Tempo: {segundos:.1f}s | Uso do CPU: {uso_cpu:.2f}% | Uso de RAM: {uso_ram:.2f}%')
 
-            writer.writerow({'Tempo (s)': segundos, 'Uso do CPU (%)': uso_cpu})
+            writer.writerow({'Tempo (s)': segundos, 'Uso do CPU (%)': uso_cpu, 'Uso de RAM (%)': uso_ram})
 
-            segundos += 1
+            segundos += 0.2
 
 if __name__ == "__main__":
     try:
-        monitorizar_cpu()
+        monitorizar_recursos()
     except KeyboardInterrupt:
-        print("\nMonitorização do CPU terminado.")
+        print("\nMonitorização terminada. Os dados foram salvos no arquivo uso_recursos.csv.")
 
